5109d02567d4c317ce7a4136c881e81724c31665
caiden nason <caidenbnason@gmail.com>
2022-10-14 16:33:52 -0400

    food rater progress

--- src/Components/WinoaFoodRater.js
+++ src/Components/WinoaFoodRater.js
@@ -5,37 +5,41 @@ import Button from 'react-bootstrap/button'
 function WinoaFoodRater(){
 
     const [foodFormData, setFoodFormData] = useState('')
+    const [foodArray, setFoodArray] = useState([])
 
     // GET WINOA FOODS
 
     function handleFoodForm(e){
         setFoodFormData(e.target.value)
-        console.log(e.target.value)
     }
 
-    function handleFoodSubmit(){
-        
+    function handleFoodArray(array){
+        setFoodArray([...foodArray, array])
+        console.log(foodArray)
     }
 
-    useEffect(() => {
-        fetch(`https://api.spoonacular.com/recipes/${foodFormData}`)
-        .then(res => res.json())
-        .then(food => console.log(food))
-    }, [])
+    function handleFoodSubmit(e){
+        e.preventDefault()
+            fetch(`https://api.spoonacular.com/food/products/search?query=${foodFormData}&apiKey=f2fd8a85f941433faa917ece2c78ad80`)
+            .then(res => res.json())
+            .then(food => handleFoodArray(food.products))
+    }
+
+
 
     return(
         <div>
-            <Form>
+            <Form onSubmit={handleFoodSubmit}>
                 <input
                 type="text"
                 name="name"
                 value={foodFormData}
                 onChange={handleFoodForm}
                 />
+            <Button type='Submit'>Submit Food</Button>
             </Form>
         </div>
     )
-
 }
 
 export default WinoaFoodRater
